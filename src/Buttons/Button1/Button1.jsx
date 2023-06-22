1c51f3872c57dbc0d1684850ea4cc315d0731093
NNVG <g696607013@gmail.com>
2023-06-22 18:54:24 +0100

    Done with the food recipe app desing and logic

--- src/Buttons/Button1/Button1.jsx
+++ src/Buttons/Button1/Button1.jsx
@@ -1,15 +1,105 @@
-import React, { useState } from 'react';
-import Modal from '../../Modals/Modal1';
+import React, { useContext, useState } from 'react';
+import { Modal } from 'react-responsive-modal';
+import formContext from '../../Context/formContext';
+import 'react-responsive-modal/styles.css';
 import './Button1.css';
 
 function Button1() {
-  const [isOpen, setIsOpen] = useState(false);
+  const { items, setItems } = useContext(formContext);
+  const [favorite] = useState('no');
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const id = Date.now();
+    const data = new FormData(e.currentTarget);
+    const values = Object.fromEntries(data.entries());
+    const itemValues = { ...values, id, favorite };
+    setItems([...items, itemValues]);
+    setOpen(false);
+  };
   return (
     <div>
-      <button onClick={() => setIsOpen(true)} className="Btn" type="button">
+      <button onClick={onOpenModal} className="Btn" type="button">
         Add New Recipe +
       </button>
-      {isOpen && <Modal setIsOpen={setIsOpen} />}
+      <Modal open={open} onClose={onCloseModal} center>
+        <div className="modalHeader">
+          <h5 className="heading">Add a new recipe!</h5>
+        </div>
+        <form onSubmit={handelSubmit}>
+          <div className="modalContent">
+            <div className="modalContentContainer">
+              <label className="modalContentLabel" htmlFor="RecipeName">
+                Recipe Name:
+                <input
+                  name="RecipeName"
+                  className="modalContentsInput"
+                  placeholder="Please enter food name"
+                  type="text"
+                  id="RecipeName"
+                  required
+                />
+              </label>
+            </div>
+            <div className="modalContentContainer">
+              <label className="modalContentLabel" htmlFor="FoodImage">
+                Food Image URL:
+                <input
+                  name="FoodImage"
+                  className="modalContentsInput"
+                  placeholder="Please enter food image URL"
+                  type="text"
+                  id="FoodImage"
+                  required
+                />
+              </label>
+            </div>
+            <div className="modalContentContainer">
+              <label className="modalContentLabel" htmlFor="Description">
+                Description:
+                <textarea
+                  className="modalContentTextarea"
+                  name="Description"
+                  id="Description"
+                  cols="30"
+                  rows="3"
+                  required
+                  placeholder="Enter food description"
+                />
+              </label>
+            </div>
+            <div className="modalContentContainer">
+              <label className="modalContentLabel" htmlFor="Ingredients">
+                Ingredients:
+                <textarea
+                  name="Ingredients"
+                  id="Ingredients"
+                  cols="30"
+                  rows="3"
+                  required
+                  placeholder="Enter food ingredients"
+                />
+              </label>
+            </div>
+          </div>
+          <div className="actionsContainers">
+            <button type="submit" className="saveBtn">
+              Add
+            </button>
+            <button
+              type="button"
+              className="cancelBtn"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 }
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable react/no-array-index-key */
 import React, { useEffect, useState } from 'react';
+import { Modal } from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
 import Button1 from '../Buttons/Button1/Button1';
 import formContext from '../Context/formContext';
 import './Home.css';
@@ -15,12 +20,87 @@ const formData = () => {
 
 function Home() {
   const [items, setItems] = useState(formData);
+  const [searchRecipe, setSearchRecipe] = useState('');
+  const [favorite, setFavorite] = useState('no');
+  const [itemId, setItemId] = useState('');
+  const [editRec, setEditRec] = useState('');
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+
+  const onCloseModal1 = () => setOpen1(false);
+  const onCloseModal2 = () => setOpen2(false);
+  const [val, setVal] = useState('');
+
+  const searchBar = () => {
+    setSearchRecipe(val);
+  };
+
+  const FavoriteRecipe = (id) => {
+    const findOdj = items.find((obj) => {
+      return obj.id === id;
+    });
+
+    if (findOdj.favorite === 'yes') {
+      findOdj.favorite = 'no';
+    } else {
+      findOdj.favorite = 'yes';
+    }
+
+    setFavorite(findOdj.favorite);
+
+    const filtered = items.filter((value) => {
+      return value.id !== id;
+    });
+
+    const update = [...filtered, findOdj];
+
+    localStorage.setItem('items', JSON.stringify(update));
+  };
+
+  const editRecipe = (id) => {
+    setOpen1(true);
+    const findRecipe = items.find((obj) => {
+      return obj.id === id;
+    });
+
+    setEditRec(findRecipe);
+  };
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+
+    const filtered = items.filter((value) => {
+      return value.id !== editRec.id;
+    });
+
+    const id = Date.now();
+    const data = new FormData(e.currentTarget);
+    const values = Object.fromEntries(data.entries());
+    const itemValues = { ...values, id, favorite, filtered };
+    const update = [...filtered, itemValues];
+
+    localStorage.setItem('items', JSON.stringify(update));
+    window.location.reload(true);
+    setOpen1(false);
+  };
+
+  console.log(editRec);
+
+  const DeleteRecipe = (id) => {
+    const deleteRecipe = items.filter((del) => {
+      return del.id !== id;
+    });
+
+    localStorage.setItem('items', JSON.stringify(deleteRecipe));
+
+    window.location.reload(true);
+  };
 
   useEffect(() => {
     localStorage.setItem('items', JSON.stringify(items));
-  }, [items]);
-
-  console.log(items);
+    setSearchRecipe('');
+    setFavorite('');
+  }, [items, val, favorite]);
 
   return (
     <formContext.Provider value={{ items, setItems }}>
@@ -28,19 +108,208 @@ function Home() {
         <header className="Header">
           <div className="Header-h1">
             <h1>Food Recipe App</h1>
-            <input type="text" placeholder="Search Recipe" />
-            <button type="button">Search</button>
+            <div>
+              <input
+                type="text"
+                className="headerInput"
+                placeholder="Search Recipe"
+                onChange={(e) => setVal(e.target.value)}
+              />
+              <button type="button" onClick={searchBar}>
+                Search
+              </button>
+            </div>
           </div>
           <Button1 />
         </header>
         <div className="bodyContainer">
           <div className="bodyContent-container">
-            {[...items].map((item, i) => (
-              <div className="bodyContent" key={i}>
-                <img src={item.FoodImage} alt="Food" />
-                <p>{item.RecipeName}</p>
+            {searchRecipe === ''
+              ? [...items].map((item, id) => (
+                  <div className="bodyContent" key={id}>
+                    <div className="container">
+                      <img src={item.FoodImage} alt="Food_Image" />
+                      <div className="overlay">
+                        <div className="recipeDetailContainer">
+                          <div className="recipeDetail">
+                            <p>Description : </p>
+                            <span>{item.Description}</span>
+                          </div>
+                          <div className="recipeDetail">
+                            <p>Ingredient : </p>
+                            <span>{item.Ingredients}</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="bodyContent-description">
+                      <p>{item.RecipeName}</p>
+                      <div className="bodyContent-icons">
+                        <i
+                          onClick={() => FavoriteRecipe(item.id)}
+                          className={
+                            item.favorite === 'yes'
+                              ? 'red-icon fa-solid fa-heart'
+                              : 'black-icon fa-regular fa-heart'
+                          }
+                        />
+                        <i
+                          onClick={() => editRecipe(item.id)}
+                          className="edit-icon fa-solid fa-pen-to-square"
+                        />
+                        <i
+                          onClick={() => {
+                            setOpen2(true);
+                            setItemId(item.id);
+                          }}
+                          className="delete-icon fa-solid fa-trash-can"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))
+              : items
+                  .filter(
+                    (search) =>
+                      search.RecipeName.toLowerCase() ===
+                      searchRecipe.toLowerCase()
+                  )
+                  .map((item, id) => (
+                    <div className="bodyContent" key={id}>
+                      <img src={item.FoodImage} alt="Food_Image" />
+                      <div className="overlay">
+                        <div className="text">
+                          <p> Description : {item.Description}</p>
+                          <p> Ingredient : {item.Ingredients}</p>
+                        </div>
+                      </div>
+                      <div className="bodyContent-description">
+                        <p>{item.RecipeName}</p>
+                        <div className="bodyContent-icons">
+                          <i
+                            onClick={() => FavoriteRecipe(item.id)}
+                            className={
+                              item.favorite === 'yes'
+                                ? 'red-icon fa-solid fa-heart'
+                                : 'black-icon fa-solid fa-heart'
+                            }
+                          />
+                          <i
+                            onClick={editRecipe}
+                            className="edit-icon fa-solid fa-pen-to-square"
+                          />
+                          <i
+                            onClick={() => setOpen2(true)}
+                            className="delete-icon fa-solid fa-trash-can"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+
+            {/* Edit Recipe Modal */}
+            <Modal open={open1} onClose={onCloseModal1} center>
+              <div className="modalHeader">
+                <h5 className="heading">Add a new recipe!</h5>
               </div>
-            ))}
+              <form onSubmit={handelSubmit}>
+                <div className="modalContent">
+                  <div className="modalContentContainer">
+                    <label className="modalContentLabel" htmlFor="RecipeName">
+                      Recipe Name:
+                      <input
+                        name="RecipeName"
+                        className="modalContentsInput"
+                        placeholder="Please enter food name"
+                        type="text"
+                        id="RecipeName"
+                        defaultValue={editRec.RecipeName}
+                        required
+                      />
+                    </label>
+                  </div>
+                  <div className="modalContentContainer">
+                    <label className="modalContentLabel" htmlFor="FoodImage">
+                      Food Image URL:
+                      <input
+                        name="FoodImage"
+                        className="modalContentsInput"
+                        placeholder="Please enter food image URL"
+                        type="text"
+                        id="FoodImage"
+                        defaultValue={editRec.FoodImage}
+                        required
+                      />
+                    </label>
+                  </div>
+                  <div className="modalContentContainer">
+                    <label className="modalContentLabel" htmlFor="Description">
+                      Description:
+                      <textarea
+                        className="modalContentTextarea"
+                        name="Description"
+                        id="Description"
+                        cols="30"
+                        rows="3"
+                        defaultValue={editRec.Description}
+                        required
+                        placeholder="Enter food description"
+                      />
+                    </label>
+                  </div>
+                  <div className="modalContentContainer">
+                    <label className="modalContentLabel" htmlFor="Ingredients">
+                      Ingredients:
+                      <textarea
+                        name="Ingredients"
+                        id="Ingredients"
+                        cols="30"
+                        rows="3"
+                        required
+                        defaultValue={editRec.Ingredients}
+                        placeholder="Enter food ingredients"
+                      />
+                    </label>
+                  </div>
+                </div>
+                <div className="actionsContainers">
+                  <button type="submit" className="saveBtn">
+                    Update
+                  </button>
+                  <button
+                    type="button"
+                    className="cancelBtn"
+                    onClick={() => setOpen1(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </form>
+            </Modal>
+
+            {/* Delete Recipe Modal */}
+            <Modal open={open2} onClose={onCloseModal2} center>
+              <p className="deleteAlert">
+                Do you really want to delete this recipe! <br />
+                <span>This process cannot be undone.</span>
+              </p>
+              <div className="actionsContainers">
+                <button
+                  type="submit"
+                  onClick={() => DeleteRecipe(itemId)}
+                  className="saveBtn"
+                >
+                  Yes
+                </button>
+                <button
+                  type="button"
+                  className="cancelBtn"
+                  onClick={() => setOpen2(false)}
+                >
+                  No
+                </button>
+              </div>
+            </Modal>
           </div>
         </div>
       </div>
